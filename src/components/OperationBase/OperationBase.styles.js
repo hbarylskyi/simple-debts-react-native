371dbaf82d81bf0238a0e47c2dc3b302d3efbd4c
Barylskyi <barylskyigb@gmail.com>
2017-09-20 20:10:14 +0300

    temp

--- src/components/OperationBase/OperationBase.styles.js
+++ src/components/OperationBase/OperationBase.styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import * as colors from '../../../colors';
+import * as colors from '../../colors';
 
 export default StyleSheet.create({
   swipeout: {
--- src/modules/actions/AuthActions.js
+++ src/modules/actions/AuthActions.js
@@ -1,19 +1,15 @@
-import { CALL_API } from "redux-api-middleware";
-import { LoginManager, AccessToken } from "react-native-fbsdk";
-import * as NavActions from "../../modules/actions/NavActions";
+import { CALL_API } from 'redux-api-middleware';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import * as NavActions from '../../modules/actions/NavActions';
 
 // TODO move redux-api-middleware calls to separate file?
-const baseUrl = "https://simple-debts.herokuapp.com";
+const baseUrl = 'https://simple-debts.herokuapp.com';
 
-export const FB_LOGIN = "FB_LOGIN";
-export const LOGIN_CHECK = "LOGIN_CHECK";
-export const LOGOUT = "LOGOUT";
+export const FB_LOGIN = 'FB_LOGIN';
+export const LOGIN_CHECK = 'LOGIN_CHECK';
+export const LOGOUT = 'LOGOUT';
 
-const loginTypes = [
-  `${FB_LOGIN}_REQUEST`,
-  `${FB_LOGIN}_SUCCESS`,
-  `${FB_LOGIN}_FAILURE`
-];
+const loginTypes = [`${FB_LOGIN}_REQUEST`, `${FB_LOGIN}_SUCCESS`, `${FB_LOGIN}_FAILURE`];
 const loginCheckTypes = [
   `${LOGIN_CHECK}_REQUEST`,
   `${LOGIN_CHECK}_SUCCESS`,
@@ -22,20 +18,20 @@ const loginCheckTypes = [
 
 const loginAction = fbToken => ({
   [CALL_API]: {
-    endpoint: baseUrl + "/auth/facebook/login",
-    method: "GET",
+    endpoint: `${baseUrl}/login/facebook`,
+    method: 'GET',
     types: loginTypes,
     headers: {
       Authorization: `Bearer ${fbToken}`
     },
-    credentials: "same-origin"
+    credentials: 'same-origin'
   }
 });
 
 const loginCheckAction = () => ({
   [CALL_API]: {
-    endpoint: baseUrl + "/login-status",
-    method: "GET",
+    endpoint: `${baseUrl}/login-status`,
+    method: 'GET',
     types: loginCheckTypes
   }
 });
@@ -45,19 +41,19 @@ const logoutAction = () => ({
 });
 
 export const fbLogin = () => dispatch => {
-  LoginManager.logInWithReadPermissions(["public_profile", "email"]).then(
+  LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(
     () => {
       AccessToken.getCurrentAccessToken().then(data => {
         dispatch(loginAction(data.accessToken.toString())).then(response => {
           if (!response.error) {
             dispatch(NavActions.goToMainScreen());
           } else {
-            alert("Login unsuccessful");
+            alert('Login unsuccessful');
           }
         });
       });
     },
-    error => alert("Login failed with error: " + error.errorMessage)
+    error => alert(`Login failed with error: ${error.errorMessage}`)
   );
 };
 
